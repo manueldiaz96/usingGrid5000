747d4be8180c369480451f77f13af5081355338f
Manuel Alejandro Díaz Zapata <18519641+manueldiaz96@users.noreply.github.com>
2019-04-27 10:16:50 +0200

    Added script with time and GPU input

---
+++ ask_for_job_scripts/ask_for_job_input_time_and_gpu.sh
@@ -0,0 +1,8 @@
+RED='\033[1;33m'
+GREEN='\033[1;32m'
+L_BLUE='\033[1;34m'
+NC='\033[0m' # No Color
+printf " ${RED}Remember to source bashrc!${NC}\n"
+printf " ${GREEN}Remember to activate the conda env!${NC}\n"
+printf " ${L_BLUE}Asking for job with $2 ${NC}\n"
+oarsub -q production -p "GPU = $2" -l "nodes=1,walltime=$1" -I
